fb4f5d5f77a23851d16a54486d3e7530a7dc2f2e
spooked03 <sietserijks@gmail.com>
2026-03-19 11:26:35 +0100

    update dotfiles

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,7 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g fish_greeting
 zoxide init fish | source
 fzf --fish | source
 
 alias yay="paru"
+alias nano="micro"
