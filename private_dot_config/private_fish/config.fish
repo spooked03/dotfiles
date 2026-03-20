f11da10f57c8906cde68f37e249a70a0417a9dad
spooked03 <sietserijks@gmail.com>
2026-03-20 20:48:41 +0100

    merge fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,9 +2,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set -g fish_greeting
 zoxide init fish | source
 fzf --fish | source
 
 alias yay="paru"
-alias nano="micro"
+#alias ff="fd | fzf --preview 'bat --color=always --style=numbers {}'"
+function ff
+  fd $argv | fzf --preview 'bat --color=always --style=numbers {}'
+end
+
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /home/nessie/.lmstudio/bin
+# End of LM Studio CLI section
+
